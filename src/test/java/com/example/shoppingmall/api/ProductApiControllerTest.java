c59f69903cc1978d158537c2b3c89af96af3a818
sejun An <asjjun@naver.com>
2023-01-07 23:43:17 +0900

    Start productApi Test Code

---
+++ src/test/java/com/example/shoppingmall/api/ProductApiControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.shoppingmall.api;
+
+import com.example.shoppingmall.service.ProductService;
+import org.junit.jupiter.api.Test;
+
+class ProductApiControllerTest extends BaseControllerTest{
+
+    ProductService productService;
+
+    @Test
+    void 상품명으로검색() throws Exception {
+
+    }
+
+    @Test
+    void findAllProduct() {
+    }
+
+    @Test
+    void findByCategory() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void editProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+}
