4a10a1a440ebb36081c3d44ce31a336d8bc5b07c
Cha Hyoung Seok <xcohdsuts@gmail.com>
2023-02-19 15:40:32 +0900

    create dockerfile

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+sudo docker ps -a -q --filter "name=shoppycicd" | grep -q . && docker stop shoppycicd && docker rm shoppycicd | true
+
+sudo docker rmi xcohdsuts/shoppycicd:1.0
+
+sudo docker pull xcohdsuts/shoppycicd:1.0
+
+# 도커 run
+docker run -d -p 8080:8080 --name shoppycicd xcohdsuts/shoppycicd:1.0
+
+# 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
+docker rmi -f $(docker images -f "dangling=true" -q) || true
